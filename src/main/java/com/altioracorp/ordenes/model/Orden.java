ffc9999b15eb01068da6e551b0be765d2adeb1d6
Vivian Cabrera <vivian.cabrera@qph.com.ec>
2024-03-14 08:03:11 -0500

    Se eliminar referencias no usadas

--- src/main/java/com/altioracorp/ordenes/model/Orden.java
+++ src/main/java/com/altioracorp/ordenes/model/Orden.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Table(name = "orden", uniqueConstraints = {
--- src/main/java/com/altioracorp/ordenes/service/impl/ArticuloServiceImpl.java
+++ src/main/java/com/altioracorp/ordenes/service/impl/ArticuloServiceImpl.java
@@ -1,12 +1,9 @@
 package com.altioracorp.ordenes.service.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-import com.altioracorp.ordenes.dto.ArticuloDto;
 import com.altioracorp.ordenes.model.Articulo;
 import com.altioracorp.ordenes.repository.ArticuloRepository;
 import com.altioracorp.ordenes.service.ArticuloService;
